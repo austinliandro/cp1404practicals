882e1eac38b6bdfc95b036313f145c7f8173408b
Austin Liandro <austin.liandro@my.jcu.edu.au>
2024-04-26 22:48:16 +0800

    completed the wiki.py file

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,15 +1,15 @@
 """
-CP1404/CP5632 Practical - AUSTIN
+CP1404 Practical
 Testing the wikipedia package
 """
 
 import wikipedia
 
-search_phrase = input("Search phrase: ")
-while search_phrase != '':
+search_query = input("Search phrase: ")
+while search_query != '':
     try:
-        searched_page = wikipedia.page(search_phrase)
-        print(f"Title: {searched_page.title}\nSummary: {wikipedia.summary(search_phrase)}URL: {searched_page.url}\n")
+        searched_page = wikipedia.page(search_query)
+        print(f"Title: {searched_page.title}\nSummary: {wikipedia.summary(search_query)}URL: {searched_page.url}\n")
     except wikipedia.exceptions.DisambiguationError as e:
         print(e.options)
     search_phrase = input("Search phrase: ")
